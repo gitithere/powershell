3fe9759bd440f421bce948763fee4cd253682ad8
gitithere <takaki.troy@gmail.com>
2017-07-26 13:05:54 -1000

    Create Get-LocalAdminMembers-Progress.ps1

---
+++ Get-LocalAdminMembers-Progress.ps1
@@ -0,0 +1,42 @@
+#  The Third section will query each computer in the ListOfComputers.txt to get the members of the local group Administrators
+#  Found @ https://stackoverflow.com/questions/21288220/get-all-local-members-and-groups-displayed-together
+#  Changed Path on Servers and Output
+#  Added Progress Bar for Servers and Members
+
+$Servers=Get-Content -Path "C:\Users\takakit\Desktop\PowerShellScripts\GetLocalGroupsAdmin\Servers.txt" 
+$output = 'C:\Users\takakit\Desktop\PowerShellScripts\GetLocalGroupsAdmin\LocalAdmins.csv'
+$results = @()
+
+$objSID = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")
+$objgroup = $objSID.Translate( [System.Security.Principal.NTAccount])
+$objgroupname = ($objgroup.Value).Split("\")[1]
+
+
+
+foreach($server in $Servers)
+{
+    for ($I = 1; $I -le 100; $I++ )
+#    {
+#    Write-Host "Powershell Script In Progress $I% Complete"; << This is wrong but, great learning experience to write out $I as a percent to the console...
+    {Write-Progress -Activity "Powershell Script In Progress-Servers " -Status "$I% Complete:" -PercentComplete $I;} ###<<<This line works just needed to comment out the For $I For Loop
+
+        $admins = @()
+        $group =[ADSI]"WinNT://$server/$objgroupname" 
+        $members = @($group.psbase.Invoke("Members"))
+        for($J = 1; $J -lt 100; $J++ )
+        {Write-Progress -Activity "Powershell Script In Progress-Members " -Status "$J% Complete:" -PercentComplete $J;}    
+            $members | foreach {
+                $obj = new-object psobject -Property @{
+                Server = $Server
+                Admin = $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+            }
+
+
+        $admins += $obj
+    } 
+    $results += $admins
+
+
+#    }
+}
+$results| Export-csv $Output -NoTypeInformation
